748a12fd3174af7cc749f1bf166c2e4d5371dba0
martinyangcp <64122341+martinyangcp@users.noreply.github.com>
2020-04-25 12:43:07 +0900

    practice problem

---
+++ 01CHAPTER_/연습문제.py
@@ -0,0 +1,19 @@
+print("환영합니다.")
+print("파이썬의 세계에 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+import turtle
+turtle.shape("turtle")
+
+lenght = 100
+angle = 90
+
+turtle.forward(lenght)
+turtle.left(angle)
+turtle.forward(lenght)
+turtle.right(angle)
+turtle.forward(lenght)
+turtle.right(angle)
+turtle.forward(lenght)
+turtle.left(angle)
+turtle.forward(lenght)
