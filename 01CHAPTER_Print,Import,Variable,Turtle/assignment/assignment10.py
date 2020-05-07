2c51ceb4c44e2f421a5909bee581097d77b474f4
martinyangcp <martinyangcp@gmail.com>
2020-05-07 14:53:41 +0900

    assignment

---
+++ 01CHAPTER_Print,Import,Variable,Turtle/assignment/assignment10.py
@@ -0,0 +1,10 @@
+import turtle
+side=200
+t=turtle
+t.shape("turtle")
+t.forward(side)
+t.left(120)
+t.forward(side)
+t.left(120)
+t.forward(side)
+t.done()
---
+++ 01CHAPTER_Print,Import,Variable,Turtle/assignment/assignment8.py
@@ -0,0 +1,2 @@
+num=int(input("반지름을 입력하시오 : "))
+print("반지름이",num,"인 원의 넓이 =", num*num*3.14)
