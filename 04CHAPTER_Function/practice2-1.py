c4e66cad4fef362fbb21fd33c58af2ab5b51f947
martinyangcp <martinyangcp@gmail.com>
2020-06-10 14:38:30 +0900

    practice

---
+++ 04CHAPTER_Function/practice2-1.py
@@ -0,0 +1,16 @@
+def math_problem(num1, num2) :
+    print("정수 " + num1 + "+" + num2 + "의 합은?")
+    
+fir = input("첫 번째 정수 :")
+sec = input("두 번째 정수 :")
+math_problem(fir, sec)
+
+'''
+import 
+
+(변수 선언)
+
+함수부
+
+main부
+'''
