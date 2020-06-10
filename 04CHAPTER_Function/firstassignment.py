0bfe3954888482b384bb1b0b038f028e2b6016d8
martinyangcp <martinyangcp@gmail.com>
2020-06-10 14:36:45 +0900

    assignment

---
+++ 04CHAPTER_Function/firstassignment.py
@@ -0,0 +1,17 @@
+first= int(input("첫 번째 수 :"))
+second= int(input("두 번째 수 :"))
+third = int(input("세 번째 수 :"))
+
+def hab (a,b,c):
+    return a+b+c
+
+
+
+def gob (a,b,c):
+    return a*b*c
+
+
+
+
+print("합의 결과 :", hab(first,second,third) )
+print("곱의 결과 :", gob(first,second,third) )
