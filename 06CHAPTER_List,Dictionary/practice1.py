ae2662a6f37d21ecbec5b89073c5b886eead214a
martinyangcp <martinyangcp@gmail.com>
2020-06-29 10:29:15 +0900

    practice

---
+++ 06CHAPTER_List,Dictionary/practice1.py
@@ -0,0 +1,11 @@
+import random
+
+#           1  2  3  4  5  6
+counters = [0, 0, 0, 0, 0, 0]
+
+for i in range(1000) :
+    ran = random.randint(0, 5)
+    counters[ran] += 1
+
+for i in range(6) :
+    print("주사위가 ", (i+1), "인 경우는", counters[i])
